73f3cf92bbcd7b8443763067c0f20e6f3ee8a38e
k1reedz <151335040+k1reedz@users.noreply.github.com>
2023-12-29 15:19:35 +0300

    Update beam_refraction.py
    добавлено отображение градусной меры лучей

--- beam_refraction.py
+++ beam_refraction.py
@@ -21,6 +21,12 @@ def beam_ref_sim():
     screen.fill('white')
     pygame.draw.rect(screen, 'grey', (0, 500, 1000, 1000))
     pygame.draw.line(screen, 'black', (0, 500), (1000, 500), 5)
+
+    font = pygame.font.Font(None, 30)
+    string_rendered = font.render(str(alpha), 1, pygame.Color('black'))
+    intro_rect = string_rendered.get_rect()
+    screen.blit(string_rendered, intro_rect)
+
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -35,7 +41,14 @@ def beam_ref_sim():
             if draw:
                 x, y = get_coords(500, 500, gamma, 1000)
                 pygame.draw.line(screen, 'orange', (500, 500), (x, y), 4)
+                string_rendered = font.render(str(round(math.degrees(gamma), 2)), 1, pygame.Color('black'))
+                intro_rect = string_rendered.get_rect()
+                intro_rect.top += 980
+                screen.blit(string_rendered, intro_rect)
 
         clock.tick(100)
         pygame.display.flip()
     pygame.quit()
+
+
+beam_ref_sim()
