15187b8cf68bd937414d878b0b303649d08d1f61
E7425 <egoroleshko555@gmail.com>
2024-01-13 20:49:19 +0300

    finished friction

--- friction.py
+++ friction.py
@@ -20,8 +20,6 @@ draw_options = pymunk.pygame_util.DrawOptions(surface)
 # настройки Pymunk
 space = pymunk.Space()
 space.gravity = 0, 8000
-segment_shape = pymunk.Segment(space.static_body, (2, HEIGHT), (100 * WIDTH, HEIGHT), 26)
-space.add(segment_shape)
 
 
 def render_inputs(inputs):
@@ -29,8 +27,7 @@ def render_inputs(inputs):
         i.render_input(surface)
 
 
-def create_square(space, pos, mass, speed, friction):
-    global segment_shape
+def create_square(space, pos, mass, speed):
     square_mass, square_size = mass, (60, 60)
     square_moment = pymunk.moment_for_box(square_mass, square_size)
     # Создается тело объекта
@@ -38,11 +35,10 @@ def create_square(space, pos, mass, speed, friction):
     square_body.position = pos
 
     # Создаем и настраиваем "фигуру" объекта
-    segment_shape.friction = friction
     square_shape = pymunk.Poly.create_box(square_body, square_size)
     square_shape.body.apply_impulse_at_local_point((mass * speed, 0), (0, 0))
     square_shape.elasticity = 0
-    square_shape.friction = 1
+    square_shape.friction = 0
     square_shape.color = [randrange(256) for i in range(4)]
 
     space.add(square_body, square_shape)
@@ -54,12 +50,15 @@ def friction_simulation():
     # Нужные значения
     obj = None
 
+
     spd_cur = Status_bar(380, 100, 120, 30)
     accel_cur = Status_bar(380, 150, 120, 30)
     distance_cur = Status_bar(380, 200, 120, 30)
     spd, m, friction = None, None, None
     values = [spd, m, friction]
     # Создаем платформу
+    segment_shape = pymunk.Segment(space.static_body, (2, HEIGHT), (100 * WIDTH, HEIGHT), 26)
+    space.add(segment_shape)
     segment_shape.elasticity = 0
     segment_shape.friction = 0.0
 
@@ -67,7 +66,7 @@ def friction_simulation():
     status.set_text("Нажмите 'k', чтобы начать")
     # Поля для ввода
     input_speed = InputVal(100, 100, 120, 30, 8, default="Скорость")
-    input_friction = InputVal(100, 150, 120, 30, 8, default="Коэфициент трения")
+    input_friction = InputVal(100, 150, 220, 30, 8, default="Коэфициент трения")
     input_mass = InputVal(250, 100, 120, 30, 8, default="Масса")
     inputs = [input_speed, input_friction, input_mass]
     run = True
@@ -92,7 +91,7 @@ def friction_simulation():
                     else:
                         try:
                             spd, friction, m = (float(i) for i in values)
-                            obj = create_square(space, (60, HEIGHT - 70), m, spd, friction)
+                            obj = create_square(space, (60, HEIGHT - 58), m, spd)
                         except ValueError:
                             status.set_text("Некорректные данные")
 
@@ -109,8 +108,8 @@ def friction_simulation():
 
         if obj is not None:
             spd_cur.set_text(str(round(obj.body.velocity[0], 2)))
-            distance_cur.set_text(str(round(obj.body.position[0] - 60, 2)))
-            accel_cur.set_text("-" + str(friction * m * 10))
+            distance_cur.set_text(str(round((obj.body.position[0] - 60), 2)))
+            accel_cur.set_text("-" + str(round(friction * m * 10, 2)))
 
         surface.fill("white")
         render_inputs(inputs)
@@ -118,12 +117,14 @@ def friction_simulation():
         for cur in (spd_cur, distance_cur, accel_cur):
             cur.render_bar(surface)
         if not pause:
+            if values.count(None) == 0 and obj is not None:
+                obj.body.velocity = (obj.body.velocity[0] - friction * m * 10 / FPS, 0)
+                if obj.body.velocity[0] <= 0:
+                    obj.body.velocity = (0, 0)
             space.step(1 / FPS)
         space.debug_draw(draw_options)
-
         pg.display.flip()
         clock.tick(FPS)
     space = pymunk.Space()
     space.gravity = 0, 8000
     draw_options = pymunk.pygame_util.DrawOptions(surface)
-friction_simulation()
