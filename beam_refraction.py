9d9bbf4fa6afc6e9e64895caa60c029f6deb0b7a
k1reedz <151335040+k1reedz@users.noreply.github.com>
2024-01-15 20:51:00 +0300

    Update beam_refraction.py
    изменен вывод ошибки

--- beam_refraction.py
+++ beam_refraction.py
@@ -14,41 +14,37 @@ def get_coords(x, y, alpha, length):
 
 
 # функция для рисования 1 угла
-def draw_alpha(screen, alpha):
-    screen.fill('white')
-    pygame.draw.rect(screen, 'grey', (0, 500, 1000, 1000))
-    pygame.draw.line(screen, 'black', (0, 500), (1000, 500), 5)
+def draw_alpha(screen, alpha, font):
     x, y = get_coords(500, 500, math.radians(alpha), 1000)
     pygame.draw.line(screen, 'red', (500, 500), (1000 - x, 1000 - y), 4)
-    font = pygame.font.Font(None, 30)
-    string_rendered = font.render(str(alpha), 1, pygame.Color('black'))
+    string_rendered = font.render(str(abs(alpha)), 1, pygame.Color('black'))
     intro_rect = string_rendered.get_rect()
     screen.blit(string_rendered, intro_rect)
 
 
 # функция для рисования 2 угла
-def draw_gamma(screen, gamma, x, y):
+def draw_gamma(screen, gamma, font):
+    x, y = get_coords(500, 500, gamma, 1000)
     pygame.draw.line(screen, 'orange', (500, 500), (x, y), 4)
-    font = pygame.font.Font(None, 30)
     string_rendered = font.render(str(round(math.degrees(gamma), 2)), 1, pygame.Color('black'), pygame.Color('grey'))
     intro_rect = string_rendered.get_rect()
-    intro_rect.top += 980
+    intro_rect.top += 970
     screen.blit(string_rendered, intro_rect)
 
 
 # функция для вывода сообщения об ошибке
-def error_message(screen):
-    font = pygame.font.Font(None, 50)
+def error_message(screen, font):
     string_rendered = font.render('ОШИБКА. НЕВЕРНЫЙ ВВОД', 1, pygame.Color('red'))
     intro_rect = string_rendered.get_rect()
     screen.blit(string_rendered, intro_rect)
 
 
 # основная функция
-def beam_ref_sim():
+def refraction_simulation():
     # параметры pygame
     size = width, height = 1000, 1000
     screen = pygame.display.set_mode(size)
+    font = pygame.font.Font(None, 50)
     run = True
     draw = False
     draw2 = False
@@ -66,21 +62,8 @@ def beam_ref_sim():
                 run = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_k:  # старт симуляции
-                    if bool(input_n1.get_text()) and bool(input_n2.get_text()) and bool(input_alpha.get_text()):
-                        if input_n1.get_text().isdigit() and input_n2.get_text().isdigit() and \
-                                input_alpha.get_text().isdigit():
-                            if 0 <= int(input_alpha.get_text()) <= 180:
-                                if input_flag:
-                                    draw = True
-                                    input_flag = False
-                                else:
-                                    draw2 = True
-                            else:
-                                error = True
-                        else:
-                            error = True
-                    else:
-                        error = True
+                    draw = True
+                    input_flag = False
 
             for i in inputs:
                 i.event_handler(event)
@@ -90,19 +73,26 @@ def beam_ref_sim():
             for i in inputs:
                 i.render_input(screen)
             if error:
-                error_message(screen)
+                error_message(screen, font)
 
         elif draw:
-            # запись введенных данных в переменные
-            alpha = float(input_alpha.get_text())  # угол падения луча
-            n1 = float(input_n1.get_text())  # показатель преломления 1 поверхности
-            n2 = float(input_n2.get_text())  # показатель преломления 2 поверхности
-            draw_alpha(screen, alpha)
-            if draw2:
-                gamma = math.asin((n1 * abs(math.sin(math.radians(alpha)))) / n2)
+            try:
+                # запись введенных данных в переменные
+                screen.fill('white')
+                pygame.draw.rect(screen, 'grey', (0, 500, 1000, 1000))
+                pygame.draw.line(screen, 'black', (0, 500), (1000, 500), 5)
+                alpha = float(input_alpha.get_text())  # угол падения луча
+                n1 = float(input_n1.get_text())  # показатель преломления 1 поверхности
+                n2 = float(input_n2.get_text())  # показатель преломления 2 поверхности
+                draw_alpha(screen, alpha, font)
+
+                gamma = math.asin((n1 * abs(math.sin(math.radians(alpha)))) / n2)  # угол преломления луча
                 x, y = get_coords(500, 500, gamma, 1000)
-                draw_gamma(screen, gamma, x, y)
+                draw_gamma(screen, gamma, font)
                 draw = False
+            except Exception:
+                input_flag = True
+                error = True
 
         pygame.display.flip()
     pygame.quit()
