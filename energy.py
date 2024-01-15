c01f3f6800529ffde4b43f2222a94fd7fb261842
k1reedz <151335040+k1reedz@users.noreply.github.com>
2024-01-15 19:27:25 +0300

    Update energy.py
    изменена проверка не ввод данных

--- energy.py
+++ energy.py
@@ -29,7 +29,7 @@ def falling_simulation():
     FPS = 60
     font = pygame.font.Font(None, 50)
     run = True
-    draw = False
+    draw = True
     error = False
     input_flag = True
     height_pix = 680    # высота падения в пикселях
@@ -46,14 +46,7 @@ def falling_simulation():
                 run = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_k:
-                    if bool(input_h.get_text()) and bool(input_m.get_text()):
-                        if input_h.get_text().isdigit() and input_m.get_text().isdigit():
-                            input_flag = False
-                            draw = True
-                        else:
-                            error = True
-                    else:
-                        error = True
+                    input_flag = False
 
             for i in inputs:
                 i.event_handler(event)
@@ -66,43 +59,48 @@ def falling_simulation():
                 error_message(screen, font)
 
         elif draw:
-            # запись введенных данных в переменные
-            h = float(input_h.get_text())   # высота падения в метрах
-            m = float(input_m.get_text())   # масса тела
+            try:
+                # запись введенных данных в переменные
+                h = float(input_h.get_text())   # высота падения в метрах
+                m = float(input_m.get_text())   # масса тела
 
-            total_t = (2 * h / 10) ** 0.5   # общее время падения
-            scale = h / height_pix          # кол-во метров в 1 пикселе
-            max_potential_e = m * 10 * h    # максимальная потенциальная энергия
+                total_t = (2 * h / 10) ** 0.5   # общее время падения
+                scale = h / height_pix          # кол-во метров в 1 пикселе
+                max_potential_e = m * 10 * h    # максимальная потенциальная энергия
 
-            screen.fill(pygame.Color('white'))
-            if t - 1 < total_t * 60:
-                s = 10 * ((t / 60) ** 2) / 2  # преодаленное расстояние в метрах
-                s_pix = int(s / scale)        # преодаленное расстояние в пикселях
-                v = t / 60 * 10               # скорость
-                if t % 30 == 0:
-                    kinetic_e = m * (v ** 2) / 2    # кинетическая энергия
-                    potential_e = m * 10 * (h - s)  # потенциальная энергия
+                if t - 1 < total_t * 60:
+                    s = 10 * ((t / 60) ** 2) / 2  # преодаленное расстояние в метрах
+                    s_pix = int(s / scale)        # преодаленное расстояние в пикселях
+                    v = t / 60 * 10               # скорость
+                    if t % 30 == 0:
+                        kinetic_e = m * (v ** 2) / 2    # кинетическая энергия
+                        potential_e = m * 10 * (h - s)  # потенциальная энергия
 
-                # вывод значений энергии объекта
-                print_text(screen, 'кинетическая энергия ' + str(int(kinetic_e)), 0, font)
-                print_text(screen, 'потенциальная энергия ' + str(int(potential_e)), 50, font)
-                print_text(screen, 'максимальная потенциальная энергия ' + str(int(max_potential_e)), 100, font)
+                    # вывод значений энергии объекта
+                    screen.fill(pygame.Color('white'))
+                    print_text(screen, 'кинетическая энергия ' + str(int(kinetic_e)), 0, font)
+                    print_text(screen, 'потенциальная энергия ' + str(int(potential_e)), 50, font)
+                    print_text(screen, 'максимальная потенциальная энергия ' + str(int(max_potential_e)), 100, font)
 
-                pygame.draw.rect(screen, 'red', (425, 150 + s_pix, 150, 150))
-                t += 1
-            else:
-                max_kinetic_e = m * (v ** 2) / 2  # максимальная кинетическая энергия
+                    pygame.draw.rect(screen, 'red', (425, 150 + s_pix, 150, 150))
+                    t += 1
 
-                # вывод значений энергии объекта
-                print_text(screen, 'кинетическая энергия ' + '0', 0, font)
-                print_text(screen, 'потенциальная энергия ' + '0', 50, font)
-                print_text(screen, 'максимальная потенциальная энергия ' + str(int(max_potential_e)), 100, font)
-                print_text(screen, 'максимальная кинетическая энергия ' + str(int(max_kinetic_e)), 150, font)
+                else:
+                    max_kinetic_e = m * (v ** 2) / 2  # максимальная кинетическая энергия
 
-                pygame.draw.rect(screen, 'red', (425, 850, 150, 150))
-                draw = False
+                    # вывод значений энергии объекта
+                    screen.fill('white')
+                    print_text(screen, 'кинетическая энергия ' + '0', 0, font)
+                    print_text(screen, 'потенциальная энергия ' + '0', 50, font)
+                    print_text(screen, 'максимальная потенциальная энергия ' + str(int(max_potential_e)), 100, font)
+                    print_text(screen, 'максимальная кинетическая энергия ' + str(int(max_kinetic_e)), 150, font)
+
+                    pygame.draw.rect(screen, 'red', (425, 860, 150, 150))
+                    draw = False
+            except Exception:
+                input_flag = True
+                error = True
 
         clock.tick(FPS)
         pygame.display.update()
     pygame.quit()
-
